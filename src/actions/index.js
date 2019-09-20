42564520cc799d24486fd4d92fd16d8d2c07276d
Josh <Joshua.horgen@gmail.com>
2019-09-20 12:56:27 -0700

    Set up Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,19 @@ export const addTodo = text => ({
   id: nextTodoId++,
   text
 })
+
+export const setVisibilityFilter = filter => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter
+})
+
+export const toggleTodo = id => ({
+  type: 'TOGGLE_TODO',
+  id
+})
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
