75ff77d59cc003d223b9df9bf5deec3b563b1734
Josh <Joshua.horgen@gmail.com>
2019-09-27 14:38:49 -0700

    Set up middleware and thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import Routes from './components/Routes'
-import IntroPage from './components/IntroPage'
+import Routes from './components/Routes';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<Provider store={configureStore()}>
+<Routes />
+</Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
