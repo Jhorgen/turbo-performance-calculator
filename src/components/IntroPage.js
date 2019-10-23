b6fa139219a8beee5f5d4437aebe88a96bb66005
Josh <Joshua.horgen@gmail.com>
2019-10-22 17:47:37 -0700

    Build slider for intro page

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import introImage  from './../assets/intro.png'
-import turbo from './../assets/turbo1.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import introImage  from './../assets/intro.png';
+import turbo from './../assets/turbo1.png';
+import { Link } from 'react-router-dom';
+import { Gallery, GalleryImage } from 'react-gesture-gallery';
+import turbo1 from './../carouselImages/turbo1.png';
+import turbo2 from './../carouselImages/turbo2.png';
+import turbo3 from './../carouselImages/turbo3.png';
+
+export const carousel = [
+  turbo1,
+  turbo2,
+  turbo3
+];
 
 function IntroPage() {
   return (
     <div>
     <div className='intro-page'>
     <Link to='/makes'>
-    <img className='turbo-image' src={turbo} />
+      <Gallery className='Intro-page' index={0}>
+        {carousel.map(image => (
+          <GalleryImage className='turbo-image' src={image} />
+        ))}
+      </Gallery>
     </Link>
     </div>
     <div className='intro-header'>
